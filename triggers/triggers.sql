fa6a53f2a8e3277aad2ad842fc38dfad02379784
Rodrigo Larenas <87916877+Ti0gorro@users.noreply.github.com>
2023-10-23 13:52:27 -0300

    Tutorial Triggers p.26

--- triggers/triggers.sql
+++ triggers/triggers.sql
@@ -23,28 +23,20 @@ END IF;
 RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
-CREATE TRIGGER trigger_log_emergencia_changes
-    AFTER INSERT OR UPDATE ON emergencia
-                        FOR EACH ROW
-                        EXECUTE FUNCTION log_emergencia_changes();
-
+   ---------------------------------------------------------------------------------------------
 CREATE OR REPLACE FUNCTION log_tarea_changes()
 RETURNS TRIGGER AS $$
-DECLARE
-estado_nombre VARCHAR(255);
 BEGIN
     IF (TG_OP = 'INSERT') THEN
         INSERT INTO coordinador_log(coordinador_id, action, table_name, record_id, timestamp)
         VALUES (NEW.coordinador_id, 'crear', 'tarea', NEW.id, now());
 RETURN NEW;
 ELSIF (TG_OP = 'UPDATE') THEN
-SELECT nombre INTO estado_nombre FROM estado_tarea WHERE id = NEW.estado_tarea_id;
-
-IF (OLD.estado_tarea_id != NEW.estado_tarea_id) THEN
-            IF (estado_nombre = 'En proceso') THEN
+        IF (OLD.estado_tarea_id != NEW.estado_tarea_id) THEN
+            IF (NEW.estado_tarea_id = 2) THEN
                 INSERT INTO coordinador_log(coordinador_id, action, table_name, record_id, timestamp)
                 VALUES (NEW.coordinador_id, 'iniciar', 'tarea', NEW.id, now());
-            ELSIF (estado_nombre = 'Completada') THEN
+            ELSIF (NEW.estado_tarea_id = 3) THEN
                 INSERT INTO coordinador_log(coordinador_id, action, table_name, record_id, timestamp)
                 VALUES (NEW.coordinador_id, 'finalizar', 'tarea', NEW.id, now());
 END IF;
@@ -54,7 +46,12 @@ END IF;
 RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
-
+---------------------------------------------
+CREATE TRIGGER trigger_log_emergencia_changes
+    AFTER INSERT OR UPDATE ON emergencia
+                        FOR EACH ROW
+                        EXECUTE FUNCTION log_emergencia_changes();
+-----------------------------------------------
 CREATE TRIGGER trigger_log_tarea_changes
     AFTER INSERT OR UPDATE ON tarea
                         FOR EACH ROW
